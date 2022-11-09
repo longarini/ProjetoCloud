af42fb62469601a63bd0c5bddd1469cdce27534b
Longarini <vlongarini@outlook.com>
2022-11-08 22:20:12 -0300

    Teste V3

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://longarini:admin@cluster0.jvvjt2m.mongodb.net/Gamificacao')
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback() {
-    console.log("MongoDB conectado.");
-});
+.then(() => {
+    console.log('MongoDB Connected…')
+  })
+  .catch(err => console.log(err))
